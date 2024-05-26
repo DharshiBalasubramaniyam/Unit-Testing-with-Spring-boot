c5860013df916b4427171c8c16d20a4303eddf74
dharshib <Dharshib.8@gmail.com>
2024-05-26 15:06:36 +0530

    add tests for save method

--- src/test/java/com/dharshi/unitTesting/repositoryTests/UserRepositoryTests.java
+++ src/test/java/com/dharshi/unitTesting/repositoryTests/UserRepositoryTests.java
@@ -5,8 +5,10 @@ import com.dharshi.unitTesting.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class UserRepositoryTests {
@@ -15,10 +17,42 @@ public class UserRepositoryTests {
     private UserRepository userRepository;
 
     @Test
-    public void testFindByEmail() {
+    @Transactional
+    @Rollback
+    public void testSaveUser() {
         // Define test data
-        String name = "test";
-        String email = "test@gmail.com";
+        String name = "test1";
+        String email = "test1@example.com";
+
+        // Create a User object with the test data
+        User user = User.builder()
+                .name(name)
+                .email(email)
+                .build();
+
+        // Save the user to the database
+        User savedUser = userRepository.save(user);
+
+        // Assert that the retrieved user is not null
+        assertNotNull(savedUser);
+
+        // Assert that the retrieved user id is not null
+        assertNotNull(savedUser.getId());
+
+        // Assert that the retrieved user's name matches the expected name
+        assertEquals(name, savedUser.getName());
+
+        // Assert that the retrieved user's email matches the expected email
+        assertEquals(email, savedUser.getEmail());
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testFindByEmailUserFound() {
+        // Define test data
+        String name = "test2";
+        String email = "test2@gmail.com";
 
         // Create a User object with the test data
         User user = User.builder()
@@ -33,13 +67,24 @@ public class UserRepositoryTests {
         User foundUser = userRepository.findByEmail(email);
 
         // Assert that the retrieved user is not null
-        assertThat(foundUser).isNotNull();
+        assertNotNull(foundUser);
 
         // Assert that the retrieved user's email matches the expected email
-        assertThat(foundUser.getEmail()).isEqualTo(email);
+        assertEquals(email, foundUser.getEmail());
 
         // Assert that the retrieved user's name matches the expected name
-        assertThat(foundUser.getName()).isEqualTo(name);
+        assertEquals(name, foundUser.getName());
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testFindByEmailUserNotFound() {
+        // Find an non existent user
+        User foundUser = userRepository.findByEmail("non.existent@example.com");
+
+        // Assert that the retrieved user is null
+        assertNull(foundUser);
     }
 
 }
